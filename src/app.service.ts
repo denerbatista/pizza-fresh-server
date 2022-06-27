d3df5928e80cc71a8eaa1e904eca285c161d47bc
denerbatista <denerbatista@icloud.com>
2022-06-26 23:05:41 -0300

    chore(app): modificated endpoint to for app status

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getAppStatus(): string {
-    return 'Server is running! 🚀\n Please check http://localhost:3001/api for Swagger docs...';
+    return 'Server is running! 🚀\n Please check https://pizza-fresh-server.onrender.com/api for Swagger docs...';
   }
 }
