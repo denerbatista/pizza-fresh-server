5cb6f41b3a305d16b28306f2ae7756d4d48ff054
denerbatista <denerbatista@icloud.com>
2022-07-18 19:45:44 -0300

    feature: config class 18

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  // Para conseguir pegar o protocolo https
+  app.set('trust proxy', 1);
 
   // Validation
   app.useGlobalPipes(new ValidationPipe());
@@ -15,18 +21,18 @@ async function bootstrap() {
     .setDescription('Aplicação para gestão das mesas de uma pizzaria')
     .setVersion('1.0.0')
     .addTag('status')
-    .addTag('auth') // < Novo código
+    .addTag('auth')
     .addTag('table')
     .addTag('product')
     .addTag('user')
     .addTag('order')
-    .addBearerAuth() // < Novo código
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3333);
 }
 
 bootstrap();
